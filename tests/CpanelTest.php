9fcdf44358c9183ad7cd4150403338151352d971
Joe Huss <detain@interserver.net>
2017-06-17 02:49:32 -0400

    adding namespace handling

--- tests/CpanelTest.php
+++ tests/CpanelTest.php
@@ -1,256 +1,257 @@
 <?php
 
+use Detain\Cpanel\Cpanel;
 use PHPUnit\Framework\TestCase;
 
 class CpanelTest extends TestCase {
 
-    /**
-     * @var Cpanel
-     */
-    protected $object;
+	/**
+	 * @var Cpanel
+	 */
+	protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new Cpanel;
-    }
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new Cpanel;
+	}
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown()
+	{
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::setopt
-     * @todo   Implement testSetopt().
-     */
-    public function testSetopt()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::setopt
+	 * @todo   Implement testSetopt().
+	 */
+	public function testSetopt()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::setCredentials
-     * @todo   Implement testSetCredentials().
-     */
-    public function testSetCredentials()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::setCredentials
+	 * @todo   Implement testSetCredentials().
+	 */
+	public function testSetCredentials()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::set_format
-     * @todo   Implement testSet_format().
-     */
-    public function testSet_format()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::set_format
+	 * @todo   Implement testSet_format().
+	 */
+	public function testSet_format()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::reactivateLicense
-     * @todo   Implement testReactivateLicense().
-     */
-    public function testReactivateLicense()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::reactivateLicense
+	 * @todo   Implement testReactivateLicense().
+	 */
+	public function testReactivateLicense()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::expireLicense
-     * @todo   Implement testExpireLicense().
-     */
-    public function testExpireLicense()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::expireLicense
+	 * @todo   Implement testExpireLicense().
+	 */
+	public function testExpireLicense()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::extendOnetimeUpdates
-     * @todo   Implement testExtendOnetimeUpdates().
-     */
-    public function testExtendOnetimeUpdates()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::extendOnetimeUpdates
+	 * @todo   Implement testExtendOnetimeUpdates().
+	 */
+	public function testExtendOnetimeUpdates()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::changeip
-     * @todo   Implement testChangeip().
-     */
-    public function testChangeip()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::changeip
+	 * @todo   Implement testChangeip().
+	 */
+	public function testChangeip()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::requestTransfer
-     * @todo   Implement testRequestTransfer().
-     */
-    public function testRequestTransfer()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::requestTransfer
+	 * @todo   Implement testRequestTransfer().
+	 */
+	public function testRequestTransfer()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::activateLicense
-     * @todo   Implement testActivateLicense().
-     */
-    public function testActivateLicense()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::activateLicense
+	 * @todo   Implement testActivateLicense().
+	 */
+	public function testActivateLicense()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::addPickupPass
-     * @todo   Implement testAddPickupPass().
-     */
-    public function testAddPickupPass()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::addPickupPass
+	 * @todo   Implement testAddPickupPass().
+	 */
+	public function testAddPickupPass()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::registerAuth
-     * @todo   Implement testRegisterAuth().
-     */
-    public function testRegisterAuth()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::registerAuth
+	 * @todo   Implement testRegisterAuth().
+	 */
+	public function testRegisterAuth()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchGroups
-     * @todo   Implement testFetchGroups().
-     */
-    public function testFetchGroups()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchGroups
+	 * @todo   Implement testFetchGroups().
+	 */
+	public function testFetchGroups()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchLicenseRiskData
-     * @todo   Implement testFetchLicenseRiskData().
-     */
-    public function testFetchLicenseRiskData()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchLicenseRiskData
+	 * @todo   Implement testFetchLicenseRiskData().
+	 */
+	public function testFetchLicenseRiskData()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchLicenseRaw
-     * @todo   Implement testFetchLicenseRaw().
-     */
-    public function testFetchLicenseRaw()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchLicenseRaw
+	 * @todo   Implement testFetchLicenseRaw().
+	 */
+	public function testFetchLicenseRaw()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchLicenseId
-     * @todo   Implement testFetchLicenseId().
-     */
-    public function testFetchLicenseId()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchLicenseId
+	 * @todo   Implement testFetchLicenseId().
+	 */
+	public function testFetchLicenseId()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchPackages
-     * @todo   Implement testFetchPackages().
-     */
-    public function testFetchPackages()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchPackages
+	 * @todo   Implement testFetchPackages().
+	 */
+	public function testFetchPackages()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchLicenses
-     * @todo   Implement testFetchLicenses().
-     */
-    public function testFetchLicenses()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchLicenses
+	 * @todo   Implement testFetchLicenses().
+	 */
+	public function testFetchLicenses()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::fetchExpiredLicenses
-     * @todo   Implement testFetchExpiredLicenses().
-     */
-    public function testFetchExpiredLicenses()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::fetchExpiredLicenses
+	 * @todo   Implement testFetchExpiredLicenses().
+	 */
+	public function testFetchExpiredLicenses()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 
-    /**
-     * @covers Detain\Cpanel\Cpanel::findKey
-     * @todo   Implement testFindKey().
-     */
-    public function testFindKey()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Detain\Cpanel\Cpanel::findKey
+	 * @todo   Implement testFindKey().
+	 */
+	public function testFindKey()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+			'This test has not been implemented yet.'
+		);
+	}
 }
