3d522a72f1a5511460727a0fae0f313fc3e5d537
Joe Huss <detain@interserver.net>
2025-02-08 06:31:00 -0500

    update

--- bin/fetch_packages.php
+++ bin/fetch_packages.php
@@ -4,6 +4,6 @@ include __DIR__.'/../../../../include/functions.inc.php';
 
 $cpl = new \Detain\Cpanel\Cpanel(CPANEL_LICENSING_USERNAME, CPANEL_LICENSING_PASSWORD);
 $cpl->format = 'json';
-$return = json_decode($cpl->fetchPackages(),true);
+$return = json_decode($cpl->fetchPackages(true),true);
 print_r($return);
 //echo json_encode($licenses, JSON_PRETTY_PRINT);
